3524c871817523198eb28c54e42d0adfe1911796
Gerson Carvalho <gecarval@c1r9s8.42luanda.com>
2024-10-23 09:49:54 +0100

    added comment to exec func

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,17 +6,16 @@
 /*   By: gecarval <gecarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 08:40:26 by gecarval          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/10/23 09:39:53 by gecarval         ###   ########.fr       */
-=======
-/*   Updated: 2024/10/22 08:43:12 by gecarval         ###   ########.fr       */
->>>>>>> parent of d11d6e9 (added a lot of comments for intro to the code && fixed all leaks until now and all segfaults && finalized the commands parsing (if more is needed it will be added soon))
+/*   Updated: 2024/10/23 09:49:17 by gecarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-pid_t	ft_fork(void)
+// This function forks the process
+// It returns the pid of the child process if it was successful
+// It prints an error message and frees the shell data if it fails
+pid_t	ft_fork(t_shell *shell)
 {
 	pid_t	pid;
 
@@ -24,72 +23,67 @@ pid_t	ft_fork(void)
 	if (pid < 0)
 	{
 		printf("minishell: fork failed\n");
+		ft_free_all(shell);
 		exit(1);
 	}
 	return (pid);
 }
 
-void	ft_exec_child(t_cmd *cmd, char **envp)
+// This function executes the child process
+// If the command is an EXEC type, it executes the command
+// If the command is a PIPE type, it executes the command and sets the input and output file descriptors
+// If the execve fails, it prints an error message and frees the shell data
+void	ft_exec_child(t_shell *shell)
 {
-	if (cmd->type == EXEC)
+	if (shell->cmd->type == EXEC)
 	{
-		if (execve(cmd->cmd, cmd->args, envp) == -1)
+		if (execve(shell->cmd->cmd, shell->cmd->args, shell->envp) == -1)
 		{
-			printf("minishell: %s: command not found\n", cmd->cmd);
+			printf("minishell: %s: command not found\n", shell->cmd->cmd);
+			ft_free_all(shell);
 			exit(1);
 		}
 	}
-	else if (cmd->type == PIPE)
+	else if (shell->cmd->type == PIPE)
 	{
-		if (dup2(cmd->fd_in, 0) == -1)
+		if (dup2(shell->cmd->fd_in, 0) == -1)
 		{
 			printf("minishell: dup2 failed\n");
+			ft_free_all(shell);
 			exit(1);
 		}
-		if (dup2(cmd->fd_out, 1) == -1)
+		if (dup2(shell->cmd->fd_out, 1) == -1)
 		{
 			printf("minishell: dup2 failed\n");
+			ft_free_all(shell);
 			exit(1);
 		}
-		if (execve(cmd->cmd, cmd->args, envp) == -1)
+		if (execve(shell->cmd->cmd, shell->cmd->args, shell->envp) == -1)
 		{
-			printf("minishell: %s: command not found\n", cmd->cmd);
+			printf("minishell: %s: command not found\n", shell->cmd->cmd);
+			ft_free_all(shell);
 			exit(1);
 		}
 	}
 }
 
-<<<<<<< HEAD
 // This function executes the command
 // It forks the process
 // If the pid is 0, it executes the child process
 // If the pid is not 0, it waits for the child process to finish
-=======
-void ft_free_all(t_shell *shell)
-{
-	if (shell->cmd != NULL)
-		free_cmd(&shell->cmd);
-	if (shell->line != NULL)
-		free(shell->line);
-}
-
->>>>>>> parent of d11d6e9 (added a lot of comments for intro to the code && fixed all leaks until now and all segfaults && finalized the commands parsing (if more is needed it will be added soon))
 void	exec_cmd(t_shell *shell)
 {
 	t_cmd	*cmd;
-	char	**envp;
+	pid_t	pid;
+	int		status;
 
 	cmd = shell->cmd;
-	envp = shell->envp;
 	while (cmd != NULL)
 	{
-		pid_t	pid;
-		int		status;
-
-		pid = ft_fork();
+		pid = ft_fork(shell);
 		if (pid == 0)
 		{
-			ft_exec_child(cmd, envp);
+			ft_exec_child(shell);
 			ft_free_all(shell);
 		}
 		else
